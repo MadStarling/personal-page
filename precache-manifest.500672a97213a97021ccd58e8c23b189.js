4da2c2140075572fba5d52ab452464ae026ce54b
MadStarling <starling.damasio@gmail.com>
2020-02-02 23:11:44 -0300

    Updates

---
+++ precache-manifest.500672a97213a97021ccd58e8c23b189.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834f4cc7af352a2e819a7a0ac244fcc3",
+    "url": "/personal-page/index.html"
+  },
+  {
+    "revision": "0dc7fe05bc0574b9afd6",
+    "url": "/personal-page/static/css/main.017fd1b2.chunk.css"
+  },
+  {
+    "revision": "e1b128752659906be117",
+    "url": "/personal-page/static/js/2.f3e30ba9.chunk.js"
+  },
+  {
+    "revision": "0dc7fe05bc0574b9afd6",
+    "url": "/personal-page/static/js/main.20115b84.chunk.js"
+  },
+  {
+    "revision": "382f2fe9c8641f7919e4",
+    "url": "/personal-page/static/js/runtime~main.acde6406.js"
+  }
+]);
